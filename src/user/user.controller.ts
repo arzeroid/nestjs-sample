442dc3d078e266e6e1cadbf16ace75b5289aa220
Puthipong Sthitpattanapongsa <puthi.sthit@gmail.com>
2020-06-17 11:24:03 +0700

    Add create and delete apis

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { DeleteResult } from 'typeorm';
+
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
     @Get()
-    getUsers():  Promise<User[]> {
-        return this.userService.findAll();                
+    getUsers(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    getUser(@Param('id') id: number): Promise<User> {
+        return this.userService.find(id);
+    }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto): CreateUserDto {
+        console.log(createUserDto);
+        const user = this.userService.create(createUserDto);
+        return;
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: number): Promise<DeleteResult> {
+        return this.userService.delete(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,30 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Repository, DeleteResult } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from 'src/dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @Inject('USER_REPOSITORY')
     private userRepository: Repository<User>,
-  ) {}
+  ) { }
 
-  async findAll(): Promise<User[]> {
+  findAll(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  find(id: number): Promise<User> {
+    return this.userRepository.findOne(id);
+  }
+
+  create(createUserDto: CreateUserDto): User {
+    const user = this.userRepository.create(createUserDto);
+
+    return
+  }
+
+  delete(id: number): Promise<DeleteResult> {
+    return this.userRepository.delete(id);
   }
 }
